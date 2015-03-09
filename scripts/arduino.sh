3bc88788f56af3c88369412368b6153db33c0072
Kazunori Kojima <kjm.kznr@gmail.com>
2015-03-09 11:13:23 +0900

    Use unstable rxtx

--- scripts/arduino.sh
+++ scripts/arduino.sh
@@ -4,13 +4,16 @@ chroot /mnt/gentoo /bin/bash <<'EOF'
 if [ -f /etc/portage/package.use ];then
   mv /etc/portage/package.use /etc/portage/package.use.migrate
   mkdir /etc/portage/package.use
+  mkdir /etc/portage/package.keywords
   mv /etc/portage/package.use.migrate /etc/portage/package.use/migrate
 else
   mkdir /etc/portage/package.use
+  mkdir /etc/portage/package.keywords
 fi
 echo x11-libs/cairo X >> /etc/portage/package.use/cairo
 echo cross-avr/gcc -sanitize > /etc/portage/package.use/avr-gcc-disable-sanitize
 echo 'USE="-cups cjk"' >> /etc/portage/make.conf
+echo 'dev-java/rxtx ~amd64' >> /etc/portage/package.keywords/cross-avr
 emerge layman libXt arduino xauth
 
 mkdir /usr/local/portage

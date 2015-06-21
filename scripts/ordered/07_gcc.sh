d05581aa2cc1df8e86955cd0338f8af5833b541e
John Mercier <moaxcp@gmail.com>
2015-06-21 15:22:25 -0400

    switching gcc to new version to fix issue with emerging virtualbox-guest-additions.

--- scripts/ordered/07_gcc.sh
+++ scripts/ordered/07_gcc.sh
@@ -4,5 +4,8 @@ echo "emerging gcc"
 echo "sys-devel/gcc ~amd64" >> /etc/portage/package.accept_keywords
 
 emerge -v sys-devel/gcc
-
+gcc-config -l
+gcc-config 2
+env-update && source /etc/profile
+emerge --oneshot libtool
 EOF

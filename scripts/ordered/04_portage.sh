12f00bea2a6b8e7ec243c400c876bd7390d4b506
John Mercier <moaxcp@gmail.com>
2015-06-20 23:00:36 -0400

    making things faster with MAKEOPTS EMERGE_DEFAULT_OPTS and --makeopts for genkernel.

--- scripts/ordered/04_portage.sh
+++ scripts/ordered/04_portage.sh
@@ -3,4 +3,6 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
 emerge-webrsync
+echo "MAKEOPTS=\"-j5 -l4\"" >> /etc/portage/make.conf
+echo "EMERGE_DEFAULT_OPTS=\"--jobs=4 --load-average=4\"" >> /etc/portage/make.conf
 EOF
--- scripts/ordered/07_kernel.sh
+++ scripts/ordered/07_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "copying kernel.config to /mnt/gentoo/tmp"
 cp $SCRIPTS/files/kernel.config /mnt/gentoo/tmp/
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
@@ -7,6 +7,6 @@ emerge sys-kernel/gentoo-sources
 emerge sys-kernel/genkernel
 cd /usr/src/linux
 mv /tmp/kernel.config .config
-genkernel --install --symlink --oldconfig all
+genkernel --install --symlink --oldconfig --makeopts="-j5 -l4" all
 emerge -c sys-kernel/genkernel
 EOF

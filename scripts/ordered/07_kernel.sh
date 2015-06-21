50b6b5cf9a313f730c547cd9f0fc05018ef62813
John Mercier <moaxcp@gmail.com>
2015-06-21 13:59:38 -0400

    changing kernel back to using oldconfig. This is defaulted in /etc/genkernel.conf

--- scripts/ordered/07_kernel.sh
+++ scripts/ordered/07_kernel.sh
@@ -5,9 +5,10 @@ cp $SCRIPTS/files/kernel.config /mnt/gentoo/tmp/
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge sys-kernel/gentoo-sources
 emerge sys-kernel/genkernel
+
 cd /usr/src/linux
 mv /tmp/kernel.config .config
-make olddefconfig
+
 genkernel --install --symlink --makeopts="-j5 -l4" all
 emerge -c sys-kernel/genkernel
 EOF

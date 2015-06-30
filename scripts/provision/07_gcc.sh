b7fc53f863b5bb12b535d7a9a1da5f5ad1556959
John Mercier <moaxcp@gmail.com>
2015-06-30 17:15:03 -0400

    changing order. stage2 will emerge gcc. Need to add keyword before stage2.

--- scripts/provision/07_gcc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "emerging gcc"
-echo "sys-devel/gcc ~amd64" >> /etc/portage/package.accept_keywords
-
-emerge -v sys-devel/gcc
-gcc-config -l
-gcc-config 2
-env-update && source /etc/profile
-emerge --oneshot libtool

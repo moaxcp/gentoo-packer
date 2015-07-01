418bad860a6187412ed435604b926dd2c3c967d4
John Mercier <moaxcp@gmail.com>
2015-07-01 07:11:38 -0400

    fixed bad quote

--- scripts/provision/03_gcc.sh
+++ scripts/provision/03_gcc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "adding unstable keyword to gcc. This fixes a problem with virtualbox-guest-additions.""
+echo "adding unstable keyword to gcc. This fixes a problem with virtualbox-guest-additions."
 echo "sys-devel/gcc ~amd64" >> /etc/portage/package.accept_keywords

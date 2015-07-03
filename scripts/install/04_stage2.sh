b99d0687ec8e415e19527d1a7a638c072fb51705
John Mercier <moaxcp@gmail.com>
2015-07-03 18:28:57 -0400

    fixing steps. error checking is exiting them.

--- scripts/install/04_stage2.sh
+++ scripts/install/04_stage2.sh
@@ -2,7 +2,6 @@
 set -e
 echo "progressing from stage 1 to stage 2"
 /usr/portage/scripts/bootstrap.sh
-echo "Now at stage 2. Listing gcc-config"
-gcc-config -l
 echo "selecting gcc-config 1"
 gcc-config 1
+gcc-config -l

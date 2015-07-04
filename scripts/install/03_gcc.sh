f05244d7cd48bd5dfca361113c7340d3ea7742d7
John Mercier <moaxcp@gmail.com>
2015-07-04 16:10:25 -0400

    updating environment

--- scripts/install/03_gcc.sh
+++ scripts/install/03_gcc.sh
@@ -2,3 +2,4 @@
 set -e
 echo "adding unstable keyword to gcc. This fixes a problem with virtualbox-guest-additions."
 echo "sys-devel/gcc ~amd64" >> /etc/portage/package.accept_keywords
+source /etc/profile

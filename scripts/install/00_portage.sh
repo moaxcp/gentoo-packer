043f738ac7bd68991c109246d68eaa7004154c2e
John Mercier <moaxcp@gmail.com>
2015-08-12 17:13:51 -0400

    changed number of jobs running for make and emerge

--- scripts/install/00_portage.sh
+++ scripts/install/00_portage.sh
@@ -5,7 +5,7 @@ emerge-webrsync
 echo "starting sync"
 emerge --sync --quiet
 echo "updating make.conf"
-echo "MAKEOPTS=\"-j9 -l8\"" >> /etc/portage/make.conf
-echo "EMERGE_DEFAULT_OPTS=\"--jobs=8 --load-average=8\"" >> /etc/portage/make.conf
+echo "MAKEOPTS=\"-j5 -l4\"" >> /etc/portage/make.conf
+echo "EMERGE_DEFAULT_OPTS=\"--jobs=4 --load-average=4\"" >> /etc/portage/make.conf
 echo "installing portage"
 emerge --verbose --oneshot portage
